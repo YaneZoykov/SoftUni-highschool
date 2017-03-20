4321abd7e535a1e379aa4adac7db88f81333404a
YaneZoykov <fddd_3@abv.bg>
2017-03-20 13:06:04 +0200

    Front page improvements
    Gallery improvements

--- main.js
+++ main.js
@@ -1,51 +1,82 @@
 class Images {
   constructor() {
-    $.get('main.html', html => {
-     $('main').html(html);
-          this.gallery = ['ivaka.jpg', 'evalakis.jpg', 'barovci.jpg', 'mutrata.jpg'];
-    this.id = 0;
-    this.path = 'images/';
-    $('.presentation .prev').prop('src', this.path + this.gallery[this.gallery.length - 1]);
-    $('.presentation .next').prop('src', this.path + this.gallery[1]);
-    $('.presentation .main-image').prop('src', this.path + this.gallery[0]);
-    });
-
+    let gallery = ['mutrata.jpg', 'ivaka.jpg', 'barovci.jpg', 'evalakis.jpg'];
+    sessionStorage.setItem('gallery', JSON.stringify(gallery));
+    this.path = 'images/gallery/';
+    this.id = sessionStorage.getItem('id') !== null ? sessionStorage.getItem('id') : 0;
+    this.loadHtml();
+  }
+  set id(id) {
+    if (id !== null) {
+      sessionStorage.setItem('id', id);
+    }
+  }
+  get id() {
+    return parseInt(sessionStorage.getItem('id'));
+  }
+  set gallery(item) {
+    let gallery = JSON.parse(sessionStorage.getItem('gallery'));
+    if (item instanceof Array) {
+      for (let piece of item) {
+        gallery.push(piece)
+      }
+    } else {
+        gallery.push(item)
+    }
+    
+    sessionStorage.setItem('gallery', JSON.stringify(gallery));
+  }
+  get gallery() {
+    return JSON.parse(sessionStorage.getItem('gallery'));
   }
   next() {
-    if (this.gallery[this.id + 1] !== undefined) {
+    if (this.id !== this.gallery.length - 1) {
       this.id++;
     } else {
       this.id = 0;
     }
-    this.changeHtml(this.id);
+    
+    this.changeHtml();
   }
   prev() {
-    if (this.gallery[this.id - 1] !== undefined) {
+    if (this.id !== 0) {
       this.id--;
     } else {
       this.id = this.gallery.length - 1;
     }
-    this.changeHtml(this.id);
+    
+    this.changeHtml();
   }
-  changeHtml(id) {
+  changeHtml() {
     let prev,
         next;
    
-    if (id === 0) {
+    if (this.id === 0) {
       prev = this.gallery[this.gallery.length - 1];
     } else {
-      prev = this.gallery[id - 1];
+      prev = this.gallery[this.id - 1];
     }
     
-    if (id === this.gallery.length - 1) {
+    if (this.id === this.gallery.length - 1) {
       next = this.gallery[0];
     } else {
-      next = this.gallery[id + 1];
+      next = this.gallery[this.id + 1];
     }
     
-    $('.presentation > .main-image').prop('src', this.path + this.gallery[id]);
+    $('.presentation > .main-image').prop('src', this.path + this.gallery[this.id]);
     $('.presentation > .prev').prop('src', this.path + prev);
     $('.presentation > .next').prop('src', this.path + next);
+  }
+  loadHtml() {
+    let prev = this.id === 0 ? this.gallery.length - 1 : this.id - 1;
+    let next = this.id === this.gallery.length - 1 ? 0 : this.id +1;
+    
+    $.get('main.html', html => {
+      $('main').html(html);
+      $('.presentation .prev').prop('src', this.path + this.gallery[prev]);
+      $('.presentation .next').prop('src', this.path + this.gallery[next]);
+      $('.presentation .main-image').prop('src', this.path + this.gallery[this.id]);
+    });
   }
 }
 
@@ -54,18 +85,19 @@ let $main = $('main');
 let $body = $('body');
 let $topMenu = $('.top-menu li');
 
+
 $topMenu.on('click', context => {
   $target = $(context.target);
   $topMenu.removeClass('active');
   $target.addClass('active');
-
-  $.get($target.data('href') + '.html', html => {
-    if($target.data('href') === 'main') {
-      new Images();
-    } else {
+  
+  if ($target.data('href') === 'main') {
+    Gallery.loadHtml();
+  } else {
+    $.get($target.data('href') + '.html', html => {
       $main.html(html);
-    }
-  });
+    });
+  }
 });
 
 $body.on('click', 'aside li', context => {
